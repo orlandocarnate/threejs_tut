742291ae682cf48c1d7cadec946946b2357cc649
Orlando Carnate <orlando.carnate@gmail.com>
2021-01-23 17:22:02 -0600

    simplify addEventListener using arrow function

--- main.js
+++ main.js
@@ -84,13 +84,19 @@ function animate() {
     renderer.render(scene, camera)
 }
 
-function onWindowResize() {
-    camera.aspect = window.innerWidth / window.innerHeight;
-    camera.updateProjectionMatrix();
-    renderer.setSize(window.innerWidth, window.innerHeight);
-}
+// function onWindowResize() {
+//     camera.aspect = window.innerWidth / window.innerHeight;
+//     camera.updateProjectionMatrix();
+//     renderer.setSize(window.innerWidth, window.innerHeight);
+// }
 
-window.addEventListener('resize', onWindowResize, false)
+// window.addEventListener('resize', onWindowResize, false)
+
+window.addEventListener('resize', () => {
+    camera.updateProjectionMatrix();
+    camera.aspect = window.innerWidth / window.innerHeight;
+    renderer.setSize(window.innerWidth, window.innerHeight);
+})
 
 init();
 animate();
